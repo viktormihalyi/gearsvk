56f4f5512583573d620f5b2517561b4747fba3df
Viktor Mihalyi <viktor.mihalyi@gmail.com>
2021-01-20 21:28:00 +0100

    travis fix

--- src/GearsPYD/GearsModuleEmbedded.cpp
+++ src/GearsPYD/GearsModuleEmbedded.cpp
@@ -1,5 +1,11 @@
 #include "GearsModuleCommon.hpp"
 
+// we are supposed to be able to import embedded modules from python, but it doesnt work
+// https://pybind11.readthedocs.io/en/stable/advanced/embedding.html#adding-embedded-modules
+//
+// tests use this embedded module
+
+
 PYBIND11_EMBEDDED_MODULE (GearsModule, m)
 {
     FillModule (m);
